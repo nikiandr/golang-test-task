71d4d9b46637a063e47fa63b888b6b71a46819bf
nikiandr <nikita.forduy@protonmail.com>
2021-12-25 00:13:32 +0200

    Loadhtmlglob add

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -16,6 +16,7 @@ func NewHandler(services *service.Service) *Handler {
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 
+	router.LoadHTMLGlob("assets/html/*")
 	router.Static("/css", "../assets/css")
 	router.GET("/", h.renderMainPage)
 	// router.POST("/add", h.createMember)
