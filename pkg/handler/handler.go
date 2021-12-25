e6647d4c62721b36cb130aca3cf6fc1094237e8f
nikiandr <nikita.forduy@protonmail.com>
2021-12-25 02:42:19 +0200

    first part of rebuild

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,25 +1,27 @@
 package handler
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
-	"github.com/nikiandr/golang-test-task/pkg/service"
+	"github.com/nikiandr/golang-test-task/pkg/model"
 )
 
-type Handler struct {
-	services *service.Service
-}
-
-func NewHandler(services *service.Service) *Handler {
-	return &Handler{services: services}
-}
-
-func (h *Handler) InitRoutes() *gin.Engine {
+func InitRoutes() *gin.Engine {
 	router := gin.New()
 
 	router.LoadHTMLGlob("assets/html/*")
-	router.Static("/css", "../assets/css")
-	router.GET("/", h.renderMainPage)
-	// router.POST("/add", h.createMember)
+	router.GET("/", func(c *gin.Context) {
+		memberList := []model.Member{}
+		c.HTML(http.StatusOK, "main.html", memberList)
+		memberList = append(memberList, model.Member{
+			Id:        len(memberList) + 1,
+			Name:      data["name"],
+			Email:     data["email"],
+			CreatedAt: time.Now().Format("2006-01-02"),
+		})
+	})
 
 	return router
 }
--- schema/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE members
-(
-    id serial primary key,
-    name varchar(255) not null unique,
-    email varchar(255) not null unique,
-    created_at DATE NOT NULL DEFAULT NOW()
-);
