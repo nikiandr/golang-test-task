cec29d097891ac893306d2b1636f6c961545151f
Nikita Fordui <nikita.forduy@protonmail.com>
2021-12-27 00:35:53 +0200

    fixed to working with js

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -2,26 +2,18 @@ package handler
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/nikiandr/golang-test-task/pkg/model"
 )
 
 func InitRoutes() *gin.Engine {
 	router := gin.New()
 
 	router.LoadHTMLGlob("assets/html/*")
-	router.GET("/", func(c *gin.Context) {
-		memberList := []model.Member{}
-		c.HTML(http.StatusOK, "main.html", memberList)
-		memberList = append(memberList, model.Member{
-			Id:        len(memberList) + 1,
-			Name:      data["name"],
-			Email:     data["email"],
-			CreatedAt: time.Now().Format("2006-01-02"),
-		})
-	})
+	router.Static("/assets", "./assets")
 
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "main.html", nil)
+	})
 	return router
 }
